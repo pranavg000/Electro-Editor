94d13959d7ac75bd568ef608d00a4cd2580ff593
amankrsingh1812 <43903675+amankrsingh1812@users.noreply.github.com>
2020-03-30 13:57:58 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -46,59 +46,73 @@ const ipcRenderer = require('electron').ipcRenderer
 
 let fileNFileObj = {};
 
-const readTitles = function(dataURL){ 
+const readTitles = function (dataURL) {
     let titles = []
     fs.readdirSync(dataURL).forEach((file, i) => {
-        if(file.split('.').length==2){
+        if (file.split('.').length == 2) {
             titles.push({
-                title: `${file}`, 
-                dir: `${dataURL}/${file}` 
+                title: `${file}`,
+                dir: `${dataURL}/${file}`
             })
         }
     })
-   return titles
+    return titles
 }
 var curObj = null;
-readTitles('allfiles').map(({title, dir}) => {
+readTitles('allfiles').map(({ title, dir }) => {
     el = document.createElement("li");
     text = document.createTextNode(`${title}`);
     el.appendChild(text)
-    el.addEventListener('click', function(e){ // clicking on sidebar titles
-        if(curObj) curObj.fileData = Buffer(mainContent.innerHTML);
-        if(!fileNFileObj[title]){
-            
+    el.addEventListener('click', function (e) { // clicking on sidebar titles
+        if (curObj) curObj.fileData = Buffer(mainContent.innerHTML);
+        if (!fileNFileObj[title]) {
+
             fileNFileObj[title] = new FileObject(dir, title);
         }
         curObj = fileNFileObj[title];
         document.getElementById('content').innerHTML = curObj.fileData.toString();
     })
+    el.setAttribute("id", title);
     document.getElementById('titles').appendChild(el)
 });
 
 var mainContent = document.getElementById('content');
 
-mainContent.addEventListener('keydown',function(e){
-    if(curObj){
-        console.log("Unsaved")
+mainContent.addEventListener('input', function (e) {
+    if (curObj && curObj.isSaved) {
+        console.log(mainContent.innerHTML.length);
+        console.log(e.key);
+        console.log("Unsaved");
+        var titleofcurobj = document.getElementById(curObj.fileName.toString());
+        var newtitle = titleofcurobj.innerHTML + "*";
+        titleofcurobj.innerHTML = newtitle;
+        console.log(newtitle);
         curObj.isSaved = false;
 
     }
-    
+
 });
 
-ipcRenderer.on('SAVE_NEEDED', function(event, arg) {
-   save_(curObj);
+ipcRenderer.on('SAVE_NEEDED', function (event, arg) {
+    var titleofcurobj = document.getElementById(curObj.fileName.toString());
+    var newtitle = titleofcurobj.innerHTML;
+    if (curObj.isSaved==false) {
+        newtitle = newtitle.slice(0, -1);
+        titleofcurobj.innerHTML = newtitle;
+        console.log(newtitle);
+    }
+    save_(curObj);
 })
 
-ipcRenderer.on('SAVE_ALL_NEEDED', function(event, arg) {
-    
- })
+ipcRenderer.on('SAVE_ALL_NEEDED', function (event, arg) {
 
-function save_(currentFileObject){
-    if(!currentFileObject.isSaved){
-        if(currentFileObject) currentFileObject.fileData = Buffer(mainContent.innerHTML);
-        fs.writeFile(currentFileObject.fullFilePath.toString(), currentFileObject.fileData, function(err){
-            if(err) throw err;
+})
+
+function save_(currentFileObject) {
+    if (!currentFileObject.isSaved) {
+        if (currentFileObject) currentFileObject.fileData = Buffer(mainContent.innerHTML);
+        fs.writeFile(currentFileObject.fullFilePath.toString(), currentFileObject.fileData, function (err) {
+            if (err) throw err;
             // ele.innerHTML = ele.innerHTML.slice(0,ele.innerHTML.length-1);
             console.log("Saved");
             curObj.isSaved = true;
